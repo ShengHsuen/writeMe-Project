925392f52ea7ff356f9d4a0588f4405449339b6e
unknown <johannagr04@hotmail.com>
2016-02-28 02:50:09 -0600

    front end argregar reglas estamento legal

--- writeMe/src/main/java/com/mett/writeMe/pojo/LegalEstablishmentPOJO.java
+++ writeMe/src/main/java/com/mett/writeMe/pojo/LegalEstablishmentPOJO.java
@@ -1,27 +1,59 @@
 package com.mett.writeMe.pojo;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import com.mett.writeMe.ejb.User;
+
 public class LegalEstablishmentPOJO {
-	private int idLegalEstablishment;
-	private String legalEst;
-	
+	private int legal_establishmentId;
+	private String description;
+	private String part;
+	private User user;
+
 	public LegalEstablishmentPOJO() {
 		super();
 	}
 
-	public int getIdLegalEstablishment() {
-		return idLegalEstablishment;
+	public int getLegal_establishmentId() {
+		return this.legal_establishmentId;
 	}
 
-	public void setIdTipoUsuario(int idLegalEstablishment) {
-		this.idLegalEstablishment = idLegalEstablishment;
+	public void setLegal_establishmentId(int legal_establishmentId) {
+		this.legal_establishmentId = legal_establishmentId;
 	}
 
-	public String getLegalEst() {
-		return legalEst;
+
+	public String getDescription() {
+		return this.description;
 	}
 
-	public void setLegalEst(String legalEst) {
-		this.legalEst = legalEst;
+	public void setDescription(String description) {
+		this.description = description;
 	}
+
+
 	
+	public String getPart() {
+		return this.part;
+	}
+
+	public void setPart(String part) {
+		this.part = part;
+	}
+
+
+	//bi-directional many-to-one association to User Mildred
+	@ManyToOne
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = null;
+	}
+
 }
--- writeMe/src/main/webapp/resources/legalEstablishment/legalEstablishment.js
+++ writeMe/src/main/webapp/resources/legalEstablishment/legalEstablishment.js
@@ -5,10 +5,59 @@ angular.module('myApp.legalEstablishment', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/legalEstablishment', {
     templateUrl: 'resources/legalEstablishment/legalEstablishment.html'
-    	//,    controller: 'legalEstablishmentCtrl'
+    	,    controller: 'legalEstablishmentCtrl'
   });
 }])
+.controller('legalEstablishmentCtrl', ['$scope', function($scope) {
+    $scope.notBlackListed = function(value) {
+      var blacklist = ['bad@domain.com','verybad@domain.com'];
+      return blacklist.indexOf(value) === -1;
+    }
 
-/*.controller('legalEstablishmentInCtrl', ['$scope','$http',function($scope,$http) {
-	
-}]);*/
+    $scope.val = 15;
+    var updateModel = function(val){
+      $scope.$apply(function(){
+        $scope.val = val;
+      });
+    };
+    angular.element("#slider").on('slideStop', function(data){
+      updateModel(data.value);
+    });
+
+    $scope.select2Number = [
+      {text:'First',  value:'One'},
+      {text:'Second', value:'Two'},
+      {text:'Third',  value:'Three'}
+    ];
+
+    $scope.list_of_string = ['tag1', 'tag2']
+    $scope.select2Options = {
+        'multiple': true,
+        'simple_tags': true,
+        'tags': ['tag1', 'tag2', 'tag3', 'tag4']  // Can be empty list.
+    };
+
+    angular.element("#LinkInput").bind('click', function (event) {
+      event.stopPropagation();
+    });
+
+    $scope.datePicker = function (start, end, label) {
+      
+    }
+}]);
+
+
+var thumbsUp = element(by.css('span.glyphicon-thumbs-up'));
+var thumbsDown = element(by.css('span.glyphicon-thumbs-down'));
+
+it('should check ng-show / ng-hide', function() {
+  expect(thumbsUp.isDisplayed()).toBeFalsy();
+  expect(thumbsDown.isDisplayed()).toBeTruthy();
+
+  element(by.model('checked')).click();
+
+  expect(thumbsUp.isDisplayed()).toBeTruthy();
+  expect(thumbsDown.isDisplayed()).toBeFalsy();
+});
+
+
