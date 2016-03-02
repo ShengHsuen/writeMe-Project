8eea8f53bbe509443ffac2ba6eb2a2ff3fba1e1c
unknown <johannagr04@hotmail.com>
2016-03-01 22:36:10 -0600

    Todos los cambios del sprint unidos

--- writeMe/src/main/webapp/resources/app.js
+++ writeMe/src/main/webapp/resources/app.js
@@ -2,13 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-	 'myApp.signin',
-	 'myApp.signup',
-	 'myApp.home',
-	 'myApp.legalEstablishment',
-	 'myApp.acceptLegalEstablishment',
-	 'ui.grid'
-  
+     'myApp.home',
+     'myApp.legalEstablishment',
+     'myApp.acceptLegalEstablishment',
+     'ui.grid'
   ])
   .config(['$routeProvider','$provide','$httpProvider', function($routeProvider,$provide,$httpProvider) {
   	$routeProvider.otherwise({redirectTo: '/home'});
