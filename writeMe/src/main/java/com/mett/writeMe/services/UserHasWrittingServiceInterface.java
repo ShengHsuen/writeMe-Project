868768ff806a3ce811683c23549daa11fb38b97e
unknown <johannagr04@hotmail.com>
2016-03-11 17:57:10 -0600

    Eliminacion

--- writeMe/src/main/java/com/mett/writeMe/services/UserHasWrittingServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.mett.writeMe.services;
-
-import com.mett.writeMe.contracts.UserHasWrittingRequest;
-
-public interface UserHasWrittingServiceInterface {
-	Boolean save(UserHasWrittingRequest ur);
-}
