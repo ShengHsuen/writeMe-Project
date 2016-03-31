9596ed66350367ae0c0315720c5bea61347e9e25
unknown <johannagr04@hotmail.com>
2016-03-31 00:09:26 -0600

    intento generar path

--- writeMe/src/main/java/com/mett/writeMe/services/GeneratePDFService.java
+++ writeMe/src/main/java/com/mett/writeMe/services/GeneratePDFService.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StringReader;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -13,69 +14,57 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.mett.writeMe.ejb.Writting;
+import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 /**
  * @author Mildred Guerra
  *	Generate PDF 
  */
 public class GeneratePDFService {
+	private static String PDF_PATH = "resources/pdfWrittings/";
+	private static String HOST_PATH = "http://localhost:8080";
+	@Autowired
+	private ServletContext servletContext;
 	   /**
 	    * Constructor
 	    */
     public GeneratePDFService(){
        
     }
-    public void generatepdf(Writting w){
-    	Document document =new Document();
-    	try {
-			FileOutputStream fichero=new  FileOutputStream(w.getName()+".pdf");
-			PdfWriter writer= PdfWriter.getInstance(document, fichero);
-			document.open();
-			Image portda = Image.getInstance(w.getImage());
-			document.add(portda);
-			PdfContentByte cd= writer.getDirectContent();
-			BaseFont bf= BaseFont.createFont(BaseFont.COURIER_BOLD, BaseFont.CP1252,BaseFont.NOT_EMBEDDED);
-			cd.setFontAndSize(bf, 12);
-			cd.beginText();
-			cd.setTextMatrix(50,590);
-			cd.showText(w.getContent());
-			cd.endText();
-			document.close();
-    	} catch (FileNotFoundException e) {
+    public void pdf(Writting w){
+    	String contenido = w.getContent();
+        OutputStream file;
+		try {
+		file = new FileOutputStream(new File(w.getName()+".pdf"));
+		Image portda = Image.getInstance(w.getImage());
+        Document document = new Document();
+        PdfWriter.getInstance(document, file);
+        document.open();
+		document.add(portda);
+//		  document.addAuthor("Real Gagnon");
+//	      document.addCreator("Real's HowTo");
+        document.addTitle(w.getName());
+        document.add(new Paragraph(w.getName()));
+        document.add(new Paragraph("Sipnosis"));
+		document.add(new Paragraph(w.getDescription()));
+        HTMLWorker htmlWorker = new HTMLWorker(document);
+        htmlWorker.parse(new StringReader(contenido));
+        document.close();
+        file.close();
+     //  String databaseFileName = HOST_PATH  + "/" + "writeMe/" + w.getName()+ ".pdf";
+      // System.out.println("direccion "+ databaseFileName);
+		} catch (DocumentException | IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (DocumentException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} 	
+		}
+
     }
-    public void ITextHelloWorld(Writting w) {
-        try {
-            OutputStream file = new FileOutputStream(new File(w.getName()+".pdf"));
+    
 
-            Document document = new Document();
-            PdfWriter.getInstance(document, file);
-            Image portda = Image.getInstance(w.getImage());
-            document.open();
-            document.addTitle(w.getName());
-            document.add(new Paragraph(w.getName()));
-			document.add(portda);
-			document.add(new Paragraph("Sipnosis"));
-			document.add(new Paragraph(w.getDescription()));
-            document.add(new Paragraph(w.getContent()));
-            document.add(new Paragraph(new Date().toString()));
-
-            document.close();
-            file.close();
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
-             
-    }
 }
