d21e27bc9a825e47b2d7cad3b840bd6d0ba1614d
jinset <mariocr1611@hotmail.com>
2016-04-10 03:34:14 -0600

    Actualizacion

--- writeMe/src/main/java/com/mett/writeMe/contracts/UserHasWrittingResponse.java
+++ writeMe/src/main/java/com/mett/writeMe/contracts/UserHasWrittingResponse.java
@@ -14,32 +14,18 @@ import com.mett.writeMe.ejb.Writting;
 public class UserHasWrittingResponse extends BaseResponse{
 	private List<UserHasWrittingPOJO> userHasWritting;
 	private UserHasWritting uhw;
-	private boolean banned;
-
-	private Date dateCreate;
-
-	private Date dateModifie;
-
-	private boolean invitationStatus;
-
-	private int linkInvitation;
-
-	private boolean statusColor;
-	
-	private boolean owner;
-
 	private User user;
-	
 	private Writting writting;
-
+	
+	public UserHasWrittingResponse() {
+		super();
+	}
 	public UserHasWritting getUhw() {
 		return uhw;
 	}
-
 	public void setUhw(UserHasWritting uhw) {
 		this.uhw = uhw;
 	}
-
 	public User getUser() {
 		return user;
 	}
@@ -56,10 +42,6 @@ public class UserHasWrittingResponse extends BaseResponse{
 		this.writting = writting;
 	}
 
-	public UserHasWrittingResponse() {
-		super();
-	}
-
 	public List<UserHasWrittingPOJO> getUserHasWritting() {
 		return userHasWritting;
 	}
@@ -67,63 +49,5 @@ public class UserHasWrittingResponse extends BaseResponse{
 	public void setUserHasWritting(List<UserHasWrittingPOJO> userHasWritting) {
 		this.userHasWritting = userHasWritting;
 	}
-
-	public boolean isBanned() {
-		return banned;
-	}
-
-	public void setBanned(boolean banned) {
-		this.banned = banned;
-	}
-
-	public Date getDateCreate() {
-		return dateCreate;
-	}
-
-	public void setDateCreate(Date dateCreate) {
-		this.dateCreate = dateCreate;
-	}
-
-	public Date getDateModifie() {
-		return dateModifie;
-	}
-
-	public void setDateModifie(Date dateModifie) {
-		this.dateModifie = dateModifie;
-	}
-
-	public boolean isInvitationStatus() {
-		return invitationStatus;
-	}
-
-	public void setInvitationStatus(boolean invitationStatus) {
-		this.invitationStatus = invitationStatus;
-	}
-
-	public int getLinkInvitation() {
-		return linkInvitation;
-	}
-
-	public void setLinkInvitation(int linkInvitation) {
-		this.linkInvitation = linkInvitation;
-	}
-
-	public boolean isStatusColor() {
-		return statusColor;
-	}
-
-	public void setStatusColor(boolean statusColor) {
-		this.statusColor = statusColor;
-	}
-
-	public boolean isOwner() {
-		return owner;
-	}
-
-	public void setOwner(boolean owner) {
-		this.owner = owner;
-	}
-	
-
 
 }
